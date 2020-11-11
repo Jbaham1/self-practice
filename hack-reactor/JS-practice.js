9dcb332f673d3b11d4811c38851a0872109542be
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-11-11 15:55:41 -0600

    Further practice with methods on objectws, arrays, and strings

--- hack-reactor/JS-practice.js
+++ hack-reactor/JS-practice.js
@@ -1,6 +1,6 @@
 var firstName = prompt("What is your first name?")
 alert("Lets get started " + name);
-confirm("Mind if I ask you a few questions?")
+confirm("Mind if I ask you a few questions " + firstName + "?")
 alert("Based off the information you give me I can give you random facts! Isn't that cool?")
 var ageNow = prompt("How old are you?")
 var num1 = Number(prompt("Can you give me your first number?"));
@@ -46,7 +46,7 @@ console.log(output);
 
 function isOldEnoughToDrinkAndDrive(age) {
     var legalToDrinkAndDrive = false
-    if (age >= 21 && legalToDrinkAndDrive === false) {
+    if (legalToDrinkAndDrive === false) {
         return "Although you are old enough to drink...It is NEVER legal to drink and drive!!";
     }
 }
@@ -59,8 +59,8 @@ function getFullName(firstName, lastName) {
     var fullName = firstName + " " + lastName + "!"
     return fullName;
 }
-
-var output = getFullName(firstName, prompt(firstName + " What is your last name?"));
+var lastName = prompt(firstName + ", what is your last name?");
+var output = getFullName(firstName, lastName);
 alert(output);
 console.log(output);
 
@@ -68,9 +68,9 @@ console.log(output);
 function checkAge(name, age) {
     // your code here
     if(age >= 21){
-        return "Since you are legally able to drink, Welcome to the party, " + name + '!';
+        return "Since you are legally able to drink, Welcome to the party, " + firstName + '!';
     } else {
-        return "Since you are not legally able to drink, we can't invite you to the party, " + name + '!' ;
+        return "Since you are not legally able to drink, we can't invite you to the party, " + firstName + '!' ;
     }
 }
 var output = checkAge(name, ageNow);
@@ -201,3 +201,73 @@ function computeAverageLengthOfWords(word1, word2){
 var output = computeAverageLengthOfWords(specialWord, dislikedWord);
 alert(output);
 console.log(output);
+//Write a function called "addFullNameProperty".
+// Given an object that has a "firstName" property and a "lastName" property, "addFullNameProperty" sets a "fullName" property on the input object, whose value is a string with the first name and last name separated by a space.
+
+function addFullNameProperty(obj) {
+    var firstName = obj['firstName'];
+    var lastName = obj['lastName'];
+
+    if (firstName && lastName) {
+        obj['fullName'] = firstName + ' ' + lastName;
+    }
+
+    return obj;
+}
+var person = {
+    firstName: firstName,
+    lastName: lastName,
+    age: ageNow
+};
+addFullNameProperty(person);
+alert(person.fullName)
+console.log(person.fullName);
+
+//Write a function called "isPersonOldEnoughToDrive".
+// Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrive" returns whether the given person is old enough to drive.
+// Notes:
+// The legal driving age in the United States is 16.
+function isPersonOldEnoughToDrive(person) {
+    // your code here
+    if(person.age >= 16){
+        return true;
+    } else {
+        return false;
+    }
+}
+var obj = {
+    age: 16
+};
+var output = isPersonOldEnoughToDrive(obj);
+console.log(output); // --> true
+
+// Write a function called "isPersonOldEnoughToVote".
+//     Given a "person" object, that contains an "age" property, "isPersonOldEnoughToVote" returns whether the given person is old enough to vote.
+//     Notes:
+// The legal voting age in the United States is 18.
+function isPersonOldEnoughToVote(person) {
+    // your code here
+    if(person.age >= 18){
+        return true;
+    } else{
+        return false;
+    }
+}
+var obj = {
+    age: 19
+};
+var output = isPersonOldEnoughToVote(obj);
+console.log(output); // --> true
+
+//Write a function called "addArrayProperty".
+//
+// Given an object, a key, and an array, "addArrayProperty" sets a new property on the object at the given key, with its value set to the given array.
+function addArrayProperty(obj, key, arr) {
+    // your code here
+    obj[key] = arr
+}
+var myObj = {};
+var myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty); // --> [1, 3]
+
