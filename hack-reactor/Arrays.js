ed1daf42ec9a10096044e2ef594d4ef8c0fb7f87
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-11-11 16:50:16 -0600

    Self practice with free online sources.

--- hack-reactor/Arrays.js
+++ hack-reactor/Arrays.js
@@ -76,3 +76,31 @@ function getElementsUpTo(array, n) {
 }
 var output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3)
 console.log(output); // --> ['a', 'b', 'c']
+//Write a function called "getAllElementsButFirst".
+// Given an array, "getAllElementsButFirst" returns an array with all the elements but the first.
+function getAllElementsButFirst(array) {
+    return array.slice(1)
+}
+var input = [1, 2, 3, 4];
+var output = getAllElementsButFirst(input);
+console.log(output); // --> [2, 3, 4]
+
+// Write a function called "getAllElementsButLast".
+//     Given an array, "getAllElementsButLast" returns an array with all the elements but the last.
+function getAllElementsButLast(array) {
+    var lastIndex = array.length -1;
+    return array.slice(0,lastIndex);
+}
+var input = [1, 2, 3, 4];
+var output = getAllElementsButLast(input);
+console.log(output); // --> [1, 2 , 3]
+
+//Write a function called "removeFromFront".
+// Given an array, "removeFromFront" returns the given array with its first element removed.
+function removeFromFront(arr) {
+    // your code here
+    arr.shift()
+    return arr;
+}
+var output = removeFromFront([1, 2, 3]);
+console.log(output); // --> [2, 3]
