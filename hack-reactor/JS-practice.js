30ef5ad0d23cd8919b977dd0c8d74a6775ecab7f
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-11-11 16:00:45 -0600

    Further practice

--- hack-reactor/JS-practice.js
+++ hack-reactor/JS-practice.js
@@ -271,3 +271,37 @@ var myArray = [1, 3];
 addArrayProperty(myObj, 'myProperty', myArray);
 console.log(myObj.myProperty); // --> [1, 3]
 
+//Write a function called "getNthElement".
+// Given an array and an integer, "getNthElement" returns the element at the given integer, within the given array.
+// Notes:
+// If the array has a length of 0, it should return 'undefined'.
+function getNthElement(array, n) {
+    // your code here
+    return array[n];
+}
+var output = getNthElement([1, 3, 5], 1);
+console.log(output); // --> 3
+
+//Write a function called "getFirstElement".
+// Given an array, "getFirstElement" returns the first element of the given array.
+// Notes:
+// If the given array has a length of 0, it should return undefined.
+function getFirstElement(array) {
+    // your code here
+    return array[0];
+}
+var output = getFirstElement([1, 2, 3, 4, 5]);
+console.log(output); // --> 1
+
+// Write a function called "getLastElement".
+//     Given an array, "getLastElement" returns the last element of the given array.
+//     Notes:
+// If the given array has a length of 0, it should return 'undefined'.
+function getLastElement(array) {
+    // your code here
+    var length = array.length -1;
+    return array[length];
+
+}
+var output = getLastElement([1, 2, 3, 4]);
+console.log(output); // --> 4
