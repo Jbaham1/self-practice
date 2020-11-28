05a86fcafd914778222081d8556f3c1c57145130
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-11-27 23:43:09 -0600

    Self practice with free online sources

--- hack-reactor/Objects.js
+++ hack-reactor/Objects.js
@@ -227,3 +227,66 @@ var obj = {
 };
 removeStringValuesLongerThan(6, obj);
 console.log(obj); // { age: 20, location: 'Texas' }
+
+//Write a function called "removeEvenValues".
+// Given an object, "removeEvenValues" removes any properties whose values are even numbers.
+// Do this in place and return the original object, do not construct a cloned object that omits the properties.
+
+function removeEvenValues(obj) {
+    // iterate over obj
+    //see if even value
+    //delete even value
+    for(var key in obj){
+        if(obj[key] % 2 === 0){
+            delete obj[key];
+        }
+    }
+}
+
+var obj = {
+    a: 2,
+    b: 3,
+    c: 4
+};
+removeEvenValues(obj);
+console.log(obj); // --> { b: 3 }
+
+//Write a function called "countNumberOfKeys".
+// Given an object, "countNumberOfKeys" returns how many properties the given object has.
+function countNumberOfKeys(obj) {
+    //iterate over obj
+    //count amount of properties for obj
+
+    var count = 0
+    for(var key in obj){
+        count += 1
+    }
+    return count
+}
+
+var obj = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+var output = countNumberOfKeys(obj);
+console.log(output); // --> 3
+
+//Write a function called "removeOddValues".
+// Given an object, "removeOddValues" removes any properties whose values are odd numbers.
+
+function removeOddValues(obj) {
+    for(var key in obj){
+        if(obj[key] % 2 === 1){
+            delete obj[key]
+        }
+    }
+}
+
+var obj = {
+    a: 2,
+    b: 3,
+    c: 4
+};
+removeOddValues(obj);
+console.log(obj); // --> { a: 2, c: 4 }
