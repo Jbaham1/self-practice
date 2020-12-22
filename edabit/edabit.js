85dea52beac146920340ab52db58749d1b85288f
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-12-22 12:15:24 -0600

    edabit js practice

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -15,3 +15,11 @@ function countTrue(arr) {
 function shiftToLeft(x, y) {
     return x * Math.pow(2,y) ;
 }
+//Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+function timeForMilkAndCookies(date) {
+    if(date.getMonth()=== 11 && date.getDate()=== 24){
+        return true;
+    } else{
+        return false;
+    }
+}
