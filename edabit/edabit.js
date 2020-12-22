f429234915ff404d2b18f378d195124992aec846
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-12-21 23:39:22 -0600

    Math.pow

--- edabit/edabit.js
+++ edabit/edabit.js
@@ -11,3 +11,7 @@ function countTrue(arr) {
     }
     return bool.length;
 }
+//Write a function that mimics (without the use of <<) the left shift operator and returns the result from the two given integers.
+function shiftToLeft(x, y) {
+    return x * Math.pow(2,y) ;
+}
